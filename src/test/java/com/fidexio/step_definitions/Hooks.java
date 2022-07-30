1eded53ca2600f8c948679311b51502ab95091bc
turhankutay <turhankutay@gmail.com>
2022-07-30 13:23:50 +0300

    fidexio-login-logout-smoke

---
+++ src/test/java/com/fidexio/step_definitions/Hooks.java
@@ -0,0 +1,26 @@
+package com.fidexio.step_definitions;
+
+import com.fidexio.utilities.Driver;
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @After
+    public void teardownScenario(Scenario scenario){
+
+        //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
+
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+        Driver.closeDriver();
+
+    }
+}
